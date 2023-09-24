85853ef95dc77e1d6c25c07c7cba7aa58a62ae28
Cliff Wells <cliff.wells@gmail.com>
2023-09-23 17:18:36 -0700

    aws help

--- bin/plugins/mod_aws.py
+++ bin/plugins/mod_aws.py
@@ -10,6 +10,9 @@
       device: MyDevice    # last part of ARN "arn:aws:iam::123456789012:mfa/MyDevice"
       expiry: 86400       # TTL for token (will prompt for MFA code upon expiry)
     tmp: ~/.tmp/          # tmp dir to cache session token (must be secure)
+
+Session will be cached in `tmp` for `expiry` seconds and you won't be prompted
+for MFA code until that time, even in multiple shells.
 """
 
 import os
