6d25174f185e5613dfb49c1090f5e80b80ed1070
Cliff Wells <cliff.wells@gmail.com>
2023-09-25 02:12:54 -0700

    use get_resolved_path

--- bin/plugins/mod_python.py
+++ bin/plugins/mod_python.py
@@ -37,9 +37,12 @@ else:
 def load(config: ConfigBox, env: dict[str, str]) -> dict[str, str]:
     "Activate Python virtualenv."
 
+    requirements: Path | None = None
+
     if _venv := config.get("venv"):
         venv: Path = get_resolved_path(_venv, env=env)
-        requirements: str | None = config.get("requirements")
+        if _req := config.get("requirements"):
+            requirements = get_resolved_path(_req, env=env)
         packages: list[str] | None = config.get("packages")
 
         env.pop("PYTHONHOME", None)
@@ -62,7 +65,7 @@ def load(config: ConfigBox, env: dict[str, str]) -> dict[str, str]:
 
 
 def initialize_venv(
-    venv: Path, requirements: str | None, packages: list[str] | None, env: dict[str, str]
+    venv: Path, requirements: Path | None, packages: list[str] | None, env: dict[str, str]
 ) -> dict[str, str]:
     "Create the virtual environment if it doesn't exist, return env vars needed for venv."
 
@@ -90,11 +93,12 @@ def initialize_venv(
         console.print(f"{Style.FINISHED}building Python virtual environment [yellow]{venv}[/]")
 
     if requirements:
-        _req: Path = Path(requirements).expanduser().resolve()
-        with console.status(f"[bold green]:white_circle:[/]Installing requirements from [yellow]{_req}[/]"):
-            installed: bool = install_requirements(venv, _req, env)
+        with console.status(
+            f"[bold green]:white_circle:[/]Installing requirements from [yellow]{requirements}[/]"
+        ):
+            installed: bool = install_requirements(venv, requirements, env)
             style: Style = (Style.SKIPPED, Style.FINISHED)[installed]
-            console.print(f"{style}installing Python requirements from [yellow]{_req}[/]")
+            console.print(f"{style}installing Python requirements from [yellow]{requirements}[/]")
 
     if packages:
         with console.status(f"[bold green]:white_circle:[/]Installing additional packages"):
