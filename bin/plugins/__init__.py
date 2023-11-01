836288ade00be293b899d8f475fdf7d6b8187271
Cliff Wells <cliff.wells@gmail.com>
2023-10-31 23:33:04 -0700

    load env after plugins to prevent env from being evaluated before plugins are loaded

--- bin/plugins/__init__.py
+++ bin/plugins/__init__.py
@@ -110,10 +110,8 @@ def load_plugin(
     data = plugin.load(config=project_config, env=process_env)
 
     if data:  # plugins can update runtime environment
-        process_env.update(data.get("env", {}) | process_env)
         project_config.update(data.get("conf", {}))
-        project_config.plugins[plugin.key].setdefault("__vars__", {})
-        project_config.plugins[plugin.key]["__vars__"].update(data.get("vars", {}))
+        project_config["env"].update(data.get("env", {}) | process_env)
 
     for fn in plugin.events.load:
         fn()
